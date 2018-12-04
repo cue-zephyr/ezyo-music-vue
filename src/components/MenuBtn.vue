0a07ec2a0cafd9bd28a8640255a387333774e478
Unknown <dj.dusky91@hotmail.com>
2018-12-04 22:34:35 +0100

    returned to vaporwave style for branch

--- src/components/MenuBtn.vue
+++ src/components/MenuBtn.vue
@@ -15,7 +15,7 @@ export default {
 .button button {
   border: none;
   background-color: transparent;
-  color: #efefef;
+  color: #272727;
   font-family: Montserrat, sans-serif;
   height: 60px;
   text-align: center;
@@ -29,26 +29,28 @@ export default {
   .button button {
     /*background-image: linear-gradient(#ddd, #888);*/
     /*background-color: #4f6e92;*/
-    background-color: #7a3b3b;
-    border: 2px solid #7f7f7f;
-    border-radius: 2px;
+    background-color: #b8b8b8;
+    border-top: 2px solid #e4e4e4;
+    border-left: 2px solid #e4e4e4;
+    border-right: 2px solid #464646;
+    border-right: 2px solid #464646;
+    border-bottom: 2px solid #747474;
     box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
-    color: #efefef;
-    font-family: Montserrat, sans-serif;
-    font-size: 1.3em;
-    width: 160px;
+    font-family: sans-serif;
+    font-size: 0.8em;
+    width: 190px;
   }
   .button button:disabled {
     /*background-color: #6a778f;*/
     /*background-color: #516781;*/
-    background-color: #4e3131;
+    background-color: #a0a0a0;
     color: #777777;
 
   }
   .button button:hover {
     /*background-image: linear-gradient(#eee, #999);*/
     /*background-color: #5084c0;*/
-    background-color: #aa5a5a;
+    background-color: #dadada;
     border: 2px solid #acacac;
     cursor: pointer;
     text-decoration: underline;
@@ -57,8 +59,11 @@ export default {
   .button button:active {
     /*background-image: linear-gradient(#aaa, #ededed);*/
     /*background-color: #9ac3f1;*/
-    background-color: #db9e81;
-    border: 2px solid #e6e1a9;
+    background-color: #f3f3f3;
+    border-top: 2px solid #464646;
+    border-left: 2px solid #464646;
+    border-right: 2px solid #e4e4e4;
+    border-right: 2px solid #e4e4e4;
     box-shadow: none;
     color: #efefef;
     margin: 2px -2px -2px 2px;
